c9c407603384771063027fa19636492959ce38a3
markusszyska <38755238+markusszyska@users.noreply.github.com>
2022-09-07 18:19:41 +0200

    ...

--- employeeAPI/src/main/java/com/szyska/employeeAPI/data/EmployeeController.java
+++ employeeAPI/src/main/java/com/szyska/employeeAPI/data/EmployeeController.java
@@ -1,5 +1,34 @@
 package com.szyska.employeeAPI.data;
 
-public class EmployeeController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class EmployeeController {
+	
+	@Autowired
+	EmployeeRepository employeeRepo;
+	
+	@GetMapping(path="/getAllEmployees", produces = {MediaType.APPLICATION_JSON_VALUE})
+	public List<Employee> getAllEmployees() {
+		return employeeRepo.findAll();
+	}
+	
+	@PostMapping(consumes=MediaType.APPLICATION_JSON_VALUE)
+	public void addEmployeeToDatabase(@RequestBody Employee employee) {
+		try {
+			this.employeeRepo.save(employee);
+		}catch(Exception e){
+			 
+		}
+	}
+	
 }
